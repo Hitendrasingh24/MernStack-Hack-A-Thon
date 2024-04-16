fc732f79368b1e149c8aafa3317dd63d94de4c2b
Hitendrasingh24 <hitendrasingh511@gmail.com>
2024-04-17 02:07:53 +0530

    Update api.js

--- client/src/apiRoutes/api.js
+++ client/src/apiRoutes/api.js
@@ -1,2 +1,2 @@
-// export const host = "https://mernstack-hack-a-thon-2.onrender.com";
-export const host = "http://localhost:8080"
+export const host = "https://mernstack-hack-a-thon-2.onrender.com";
+// export const host = "http://localhost:8080"
