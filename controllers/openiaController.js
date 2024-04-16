7eeb9ab6e72d6311b9574cf6f7291a7c2370f9b4
Hitendrasingh24 <hitendrasingh511@gmail.com>
2024-04-16 17:38:23 +0530

    Update openiaController.js

--- controllers/openiaController.js
+++ controllers/openiaController.js
@@ -31,53 +31,5 @@ exports.summaryController = async (req, res) => {
 
 
 
-// const Summary = require('../models/Summary');
-
-// // Controller function to save a new summary string to the array in the summary model
-// const saveSummaryString = async (req, res) => {
-//   const { _id } = req.params; // Assuming _id is passed as a route parameter
-//   const { summary } = req.body;
-
-//   try {
-//     const existingSummary = await Summary.findById(_id);
-//     if (!existingSummary) {
-//       return res.status(404).json({ error: 'Summary not found' });
-//     }
-
-//     existingSummary.summaryArray.push(summary);
-//     await existingSummary.save();
-
-//     res.status(201).json({ message: 'Summary string saved successfully' });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// };
-
-// module.exports = { saveSummaryString };
-
-
-
-// const Summary = require('../models/Summary');
-
-// // Controller function to get the array of summary strings from the summary model
-// const getSummaryStrings = async (req, res) => {
-//   const { _id } = req.params; // Assuming _id is passed as a route parameter
-
-//   try {
-//     const existingSummary = await Summary.findById(_id);
-//     if (!existingSummary) {
-//       return res.status(404).json({ error: 'Summary not found' });
-//     }
-
-//     const summaryArray = existingSummary.summaryArray;
-//     res.status(200).json({ summaryArray });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// };
-
-// module.exports = { getSummaryStrings };
 
 
